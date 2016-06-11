28d25dc1c450b1964897d6dfc99b040d578c430f
Slavone <slavone666@gmail.com>
2016-06-11 23:57:48 +0300

    slim gem and prettified crawler log

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'jbuilder', '~> 2.0'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem 'slim-rails', '~> 3.1'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/services/diamond_crawler.rb
+++ app/services/diamond_crawler.rb
@@ -6,31 +6,39 @@ class DiamondCrawler
   end
 
   def test_process(count = nil, go_anyway = false)
-    @logger.info '---------------------------------'
-    @logger.info 'DiamondCrawler started'
+    log '---------------------------------'
+    log 'DiamondCrawler started'
     new_releases = current_week_releases
     date = wednesday_date new_releases
-    @logger.info "Listed date is #{date}"
+    log "Listed date is #{date}"
     if wl = check_for_weekly_list_in_db(date)
       if !go_anyway && wl.list == new_releases
-        @logger.info 'List wasnt updated, finishing process'
+        log 'List wasnt updated, finishing process'
         return
       else
-        @logger.info "List was updated"
+        log "List was updated"
         wl.update_column :list, new_releases
       end
     else
       WeeklyList.create list: new_releases, wednesday_date: date
-      @logger.info "Created new WeeklyList"
+      log "Created new WeeklyList"
     end
     diamond_ids = comics_diamond_ids new_releases 
     count ||= diamond_ids.size
-    @logger.info 'Starting crawling process'
+    log 'Starting crawling process'
     scrape_data diamond_ids.first(count)
-    @logger.info 'Finished crawling process'
+    log 'Finished crawling process'
   end
 
   private
+
+  def log(message)
+    @logger.info message
+  end
+
+  def pretty_comic_log_message(comic)
+    comic.select { |k, _| k != :preview }.map { |_, v| v }.join('|')
+  end
 
   def current_week_releases
     @parser.get_page DiamondComicsParser::CURRENT_WEEK
@@ -52,7 +60,7 @@ class DiamondCrawler
     diamond_ids.each do |diamond_id|
       comic_page = @parser.get_comic_page diamond_id
       comic = @parser.parse_comic_info comic_page
-      @logger.info comic.inspect
+      log pretty_comic_log_message(comic)
       @db_saver.persist_to_db comic
     end
   end
