e53f3ad92053348916a1525e71e781a798386451
Slavone <slavone666@gmail.com>
2016-06-11 22:37:55 +0300

    logging for diamond crawler

--- app/services/diamond_crawler.rb
+++ app/services/diamond_crawler.rb
@@ -2,34 +2,32 @@ class DiamondCrawler
   def initialize
     @parser = DiamondComicsParser.new
     @db_saver = DBSaver.new
+    @logger = Logger.new "#{Rails.root}/log/diamond_crawler.log"
   end
 
   def test_process(count = nil, go_anyway = false)
-    #TODO proper logging
+    @logger.info '---------------------------------'
+    @logger.info 'DiamondCrawler started'
     new_releases = current_week_releases
     date = wednesday_date new_releases
+    @logger.info "Listed date is #{date}"
     if wl = check_for_weekly_list_in_db(date)
       if !go_anyway && wl.list == new_releases
-        puts '--------------'
-        puts 'Nothing changed, process finished'
-        puts '--------------'
+        @logger.info 'List wasnt updated, finishing process'
         return
       else
-        puts '--------------'
-        puts 'Updated current weekly_list'
-        puts '--------------'
+        @logger.info "List was updated"
         wl.update_column :list, new_releases
       end
     else
       WeeklyList.create list: new_releases, wednesday_date: date
+      @logger.info "Created new WeeklyList"
     end
     diamond_ids = comics_diamond_ids new_releases 
     count ||= diamond_ids.size
-    puts '--------------'
-    puts 'Started crawling'
-    collect_info_for diamond_ids.first(count)
-    puts 'Finished crawling'
-    puts '--------------'
+    @logger.info 'Starting crawling process'
+    scrape_data diamond_ids.first(count)
+    @logger.info 'Finished crawling process'
   end
 
   private
@@ -50,10 +48,11 @@ class DiamondCrawler
     @parser.parse_diamond_codes(page, :comics)
   end
 
-  def collect_info_for(diamond_ids)
+  def scrape_data(diamond_ids)
     diamond_ids.each do |diamond_id|
       comic_page = @parser.get_comic_page diamond_id
       comic = @parser.parse_comic_info comic_page
+      @logger.info comic.inspect
       @db_saver.persist_to_db comic
     end
   end
