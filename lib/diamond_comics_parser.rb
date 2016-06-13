3685b49a240bce81b6960676f5e70943c6aeb945
Slavone <slavone666@gmail.com>
2016-06-13 18:15:34 +0300

    whitelisted '` symbols in title parsing

--- lib/diamond_comics_parser.rb
+++ lib/diamond_comics_parser.rb
@@ -133,7 +133,7 @@ class DiamondComicsParser
     #when 'hardcover', 'softcover', 'trade_paperback'
     #when 'graphic_novel'
     when 'single_issue'
-      matched = desc.match /(?<title>[\-\w\s.,&$]+)#(?<number>\d+)/i
+      matched = desc.match /(?<title>[\-\w\s.,&$`']+)#(?<number>\d+)/i
       return matched[:title].strip if matched
     else
       return desc
