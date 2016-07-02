071eb5239bd4a4b9b7fa491172d8c4a927e02d51
Slavone <slavone666@gmail.com>
2016-07-02 20:14:38 +0300

    scopes for quering comics by creators

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -34,10 +34,63 @@ class Comic < ApplicationRecord
   has_many :cover_artist_credits
   has_many :cover_artists, -> { order(:name) }, through: :cover_artist_credits, source: :creator
 
+  def self.build_creators_query(creators)
+    if creators.kind_of? String
+      "'#{creators}'"
+    else
+      creators.map { |c| "'#{c}'"}.join(',')
+    end
+  end
+
+  scope :filtered_by_creators, ->(creators) do
+    creators_query = Comic.build_creators_query creators
+    find_by_sql "WITH filtered_creators AS (
+                SELECT id
+                FROM creators
+                WHERE
+                name IN (#{creators_query})
+                )
+                SELECT DISTINCT comics.*
+                FROM comics
+                WHERE
+                id IN (
+                  SELECT writer_credits.comic_id
+                  FROM writer_credits JOIN filtered_creators
+                  ON writer_credits.creator_id = filtered_creators.id
+                  UNION ALL
+                  SELECT artist_credits.comic_id
+                  FROM artist_credits JOIN filtered_creators
+                  ON artist_credits.creator_id = filtered_creators.id
+                  UNION ALL
+                  SELECT cover_artist_credits.comic_id
+                  FROM cover_artist_credits JOIN filtered_creators
+                  ON cover_artist_credits.creator_id = filtered_creators.id
+                )"
+  end
+
+  scope :filtered_by_creators_of_type, ->(creators, type) do
+    creators_query = Comic.build_creators_query creators
+    find_by_sql "WITH filtered_creators AS (
+                SELECT id
+                FROM creators
+                WHERE
+                name IN (#{creators_query})
+                )
+                SELECT DISTINCT comics.*
+                FROM comics
+                WHERE
+                id IN (
+                  SELECT #{type}_credits.comic_id
+                  FROM #{type}_credits JOIN filtered_creators
+                  ON #{type}_credits.creator_id = filtered_creators.id
+                )"
+  end
+
   def humanized_title
     (title +
      "#{' #' + issue_number.to_s if item_type == 'single_issue'}" +
      "#{' VARIANT' if is_variant}" +
      "#{" #{reprint_number} PRINTING" if reprint_number && reprint_number > 1}").strip
   end
+
 end
