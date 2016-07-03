9f29e7ea83b70bf821999812eed157bc9d53e1cc
Slavone <slavone666@gmail.com>
2016-07-03 23:52:36 +0300

    more deploy fixes

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module GrepComics
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths += %W(lib)
-    config.autoload_paths += %W(app/services)
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
