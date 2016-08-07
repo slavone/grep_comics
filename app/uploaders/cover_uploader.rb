79c7ce42b01cc22eaf39a18ff372a8036354de76
Slavone <slavone666@gmail.com>
2016-08-07 15:37:13 +0300

    proper image optimization

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -9,5 +9,19 @@ class CoverUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+	def optimize
+    manipulate! do |img|
+      return img unless img.mime_type.match /image\/jpeg/
+      img.strip
+      img.combine_options do |c|
+        c.quality "80"
+        c.depth "8"
+        c.interlace "plane"
+      end
+      img
+    end
+  end
+
   process resize_to_fit: [215, 330]
+  process :optimize
 end
