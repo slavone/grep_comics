20701dc75716224e9f1816c019c0e38fe2536628
Vyacheslav Savelyev <slavone666@gmail.com>
2016-08-28 19:44:22 +0300

    configured https

--- config/schedule.rb
+++ config/schedule.rb
@@ -17,4 +17,5 @@ job_type :rbenv_runner, RBENV_INIT + "cd :path && bin/rails runner -e :environme
 every 1.day, at: '0am' do
   rbenv_runner "DiamondCrawler.new.start_process(DiamondComicsParser::CURRENT_WEEK)", environment: 'production'
   rbenv_runner "DiamondCrawler.new.retry_for_updates(WeeklyList.current_week_list)", environment: 'production'
+  command '/usr/bin/certbot renew --quiet'
 end
