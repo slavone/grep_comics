9e60084d70252aede2c07f93e554ee604b8ccccf
Vyacheslav Savelyev <slavone666@gmail.com>
2016-10-29 00:01:35 +0300

    fixed fabricator failing

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -38,9 +38,9 @@ class Comic < ApplicationRecord
   #has_many :cover_artists, -> { order(:name) }, through: :cover_artist_credits, source: :creator
 
   has_many :creator_credits
-  has_many :writer_credits, -> { where(credited_as: :writer) }, class_name: 'CreatorCredit'
-  has_many :artist_credits, -> { where(credited_as: :artist) }, class_name: 'CreatorCredit'
-  has_many :cover_artist_credits, -> { where(credited_as: :cover_artist) }, class_name: 'CreatorCredit'
+  has_many :writer_credits, -> { where(credited_as: :writer) }, class_name: 'CreatorCredit', inverse_of: :comic
+  has_many :artist_credits, -> { where(credited_as: :artist) }, class_name: 'CreatorCredit', inverse_of: :comic
+  has_many :cover_artist_credits, -> { where(credited_as: :cover_artist) }, class_name: 'CreatorCredit', inverse_of: :comic
   has_many :writers, -> { order(:name) }, through: :writer_credits, source: :creator
   has_many :artists, -> { order(:name) }, through: :artist_credits, source: :creator
   has_many :cover_artists, -> { order(:name) }, through: :cover_artist_credits, source: :creator
--- app/models/creator.rb
+++ app/models/creator.rb
@@ -17,9 +17,9 @@ class Creator < ApplicationRecord
   #has_many :comics_as_cover_artist, through: :cover_artist_credits, source: :comic
 
   has_many :creator_credits
-  has_many :writer_credits, -> { where(credited_as: :writer) }, class_name: 'CreatorCredit'
-  has_many :artist_credits, -> { where(credited_as: :artist) }, class_name: 'CreatorCredit'
-  has_many :cover_artist_credits, -> { where(credited_as: :cover_artist) }, class_name: 'CreatorCredit'
+  has_many :writer_credits, -> { where(credited_as: :writer) }, class_name: 'CreatorCredit', inverse_of: :creator
+  has_many :artist_credits, -> { where(credited_as: :artist) }, class_name: 'CreatorCredit', inverse_of: :creator
+  has_many :cover_artist_credits, -> { where(credited_as: :cover_artist) }, class_name: 'CreatorCredit', inverse_of: :creator
   has_many :comics_as_writer, through: :writer_credits, source: :comic
   has_many :comics_as_artist, through: :artist_credits, source: :comic
   has_many :comics_as_cover_artist, through: :cover_artist_credits, source: :comic
