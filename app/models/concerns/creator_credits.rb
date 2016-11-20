9198b6f8cfe9362ffc079ecaabc0a544c3555cac
Vyacheslav Savelyev <slavone666@gmail.com>
2016-11-20 15:23:21 +0300

    fix dependent: :destroy on weekly_list and creator_credits

--- app/models/concerns/creator_credits.rb
+++ app/models/concerns/creator_credits.rb
@@ -2,7 +2,7 @@ module CreatorCredits
   def self.included(klass)
     klass_name = klass.name.downcase.to_sym
 
-    klass.has_many :creator_credits
+    klass.has_many :creator_credits, dependent: :destroy
     klass.has_many :writer_credits, -> { where(credited_as: :writer) }, class_name: 'CreatorCredit', inverse_of: klass_name
     klass.has_many :artist_credits, -> { where(credited_as: :artist) }, class_name: 'CreatorCredit', inverse_of: klass_name
     klass.has_many :cover_artist_credits, -> { where(credited_as: :cover_artist) }, class_name: 'CreatorCredit', inverse_of: klass_name
--- app/models/weekly_list.rb
+++ app/models/weekly_list.rb
@@ -10,7 +10,7 @@
 #
 
 class WeeklyList < ApplicationRecord
-  has_many :comics
+  has_many :comics, dependent: :destroy
   has_many :comics_with_no_covers, -> { where no_cover_available: true }, class_name: 'Comic'
 
   class << self
